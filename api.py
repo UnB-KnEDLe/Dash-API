c418907039f0c416d522d35f296c87656ec2a02c
Jônatas G <jonatasgeek@gmail.com>
2021-09-28 12:27:17 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from dodfminer.extract.pure.core import ContentExtractor
 from dodfminer.extract.polished.core import ActsExtractor
-import json, os
+import json, os, pandas as pd
 
 app = Flask(__name__)
 cors = CORS(app, CORS_ORIGINS="localhost:5000")
@@ -15,7 +15,7 @@ def extract_content():
         type = request.form['type']
     except:
         type = 'regex'
-    
+
     f = request.files['file']
     f.save(f.filename)
 
@@ -25,17 +25,24 @@ def extract_content():
         temp_text.write(text)
 
         acts_dfs = ActsExtractor.get_all_df('tmp_txt.txt', type)
-
+    
         os.remove(f.filename)
         os.remove('tmp_txt.txt')
 
         return_list = []
         for act_name in acts_dfs:
             df = acts_dfs[act_name]
-            for list_ in df.values.tolist():
-                return_list = return_list + list_
+            df = df.where(pd.notnull(df), None) # Remove NaN
+            df_list = df.values.tolist()
+            if len(df_list) > 0:
+                for item in df_list:
+                    del item[0]
+                return_list.append({
+                    'content': df_list,
+                    'title': act_name
+                })
 
-        return json.dumps(return_list)
+        return jsonify(return_list)
         
     return 'Not a pdf file', 400
 
